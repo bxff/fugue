8578a4791f60e1d4eae57effb95f229960b447e9
bxff <51504045+bxff@users.noreply.github.com>
2026-02-21 01:52:45 +0530

    wip: revert all previous attempt leftovers

--- fugue-max-simple/src/index.ts
+++ fugue-max-simple/src/index.ts
@@ -88,7 +88,7 @@ class Tree<T> {
     value: T,
     parent: Node<T>,
     side: "L" | "R",
-    rightOrigin?: Node<T> | null
+    rightOriginID?: ID | null
   ) {
     const node: Node<T> = {
       id,
@@ -100,7 +100,9 @@ class Tree<T> {
       rightChildren: [],
       size: 0,
     };
-    if (rightOrigin !== undefined) node.rightOrigin = rightOrigin;
+    if (rightOriginID !== undefined) {
+      node.rightOrigin = rightOriginID === null ? null : this.getByID(rightOriginID);
+    }
 
     // Add to nodesByID.
     let bySender = this.nodesByID.get(id.sender);
@@ -278,7 +280,7 @@ class Tree<T> {
    */
   leftmostDescendant(node: Node<T>): Node<T> {
     let desc = node;
-    for (; desc.leftChildren.length !== 0; desc = desc.leftChildren[0]) {}
+    for (; desc.leftChildren.length !== 0; desc = desc.leftChildren[0]) { }
     return desc;
   }
 
@@ -534,7 +536,8 @@ export class FugueMaxSimple<T> extends CPrimitive {
           msg.id,
           msg.value,
           this.tree.getByID(msg.parent),
-          msg.side
+          msg.side,
+          msg.rightOrigin
         );
         // In a production implementation, we would emit an Insert event here.
         break;
